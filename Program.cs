2dbed7a92821e92911c05843a2c8bfd63d9db02d
Mark McArthey <mmcarthey@wctc.edu>
2024-05-02 21:46:49 -0500

    added ability to exclude based on selection using spectre.console

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿using Google.OrTools.Sat;
 using SeaOfConquest;
+using Spectre.Console;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
         var heroes = ReadHeroes("heroes.csv");
-        //heroes = SelectSubsetOfHeroes(heroes);
-
         var ships = GetDistinctShips(heroes);
+        var positions = new List<string> { "Captain", "First Mate", "Gunner" };
+
+        //heroes = SelectSubsetOfHeroes(heroes);
         //ships = SelectSubsetOfShips(ships);
 
-        var positions = new List<string> { "Captain", "First Mate", "Gunner" };
+        // Let the user select heroes and ships to exclude and filter them out
+        var excludedHeroes = SelectExclusionsForHeroes(heroes);
+        var excludedShips = SelectExclusionsForShips(ships);
+        heroes = heroes.Except(excludedHeroes).ToList();
+        ships = ships.Except(excludedShips).ToList();
 
         var model = new CpModel();
         var assignments = CreateAssignmentVariables(model, heroes, ships, positions);
@@ -35,6 +41,38 @@ internal class Program
         model.Maximize(totalScore);
 
         SolveModel(model, assignments);
+    }
+
+    public static List<Hero> SelectExclusionsForHeroes(List<Hero> heroes)
+    {
+        // Use Spectre.Console to create a checklist for heroes
+        var exclusions = AnsiConsole.Prompt(
+            new MultiSelectionPrompt<string>()
+                .Title("Select the heroes you want to [red]exclude[/] from the list:")
+                .NotRequired()
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more heroes)[/]")
+                .InstructionsText("[grey](Press [blue]<space>[/] to toggle a hero, [green]<enter>[/] to accept)[/]")
+                .AddChoices(heroes.Select(h => h.Name)));
+
+        // Return the list of Hero objects that are selected for exclusion
+        return heroes.Where(hero => exclusions.Contains(hero.Name)).ToList();
+    }
+
+    public static List<string> SelectExclusionsForShips(List<string> ships)
+    {
+        // Use Spectre.Console to create a checklist for ships
+        var exclusions = AnsiConsole.Prompt(
+            new MultiSelectionPrompt<string>()
+                .Title("Select the ships you want to [red]exclude[/] from the list:")
+                .NotRequired()
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more ships)[/]")
+                .InstructionsText("[grey](Press [blue]<space>[/] to toggle a ship, [green]<enter>[/] to accept)[/]")
+                .AddChoices(ships));
+
+        // Return the list of strings that are selected for exclusion
+        return exclusions;
     }
 
     private static Dictionary<string, BoolVar> CreateShipActiveVariables(CpModel model, List<string> ships)
